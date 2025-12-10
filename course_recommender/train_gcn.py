6b20663da09abc28fad5534d75e93e047761a872
Jotsna22 <122271671+Jotsna22@users.noreply.github.com>
2025-12-09 21:06:27 -0600

    Update train_gcn.py

--- course_recommender/train_gcn.py
+++ course_recommender/train_gcn.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+import datetime
 from torch.utils.data import DataLoader
 
 # Import model + utilities from recsys app
